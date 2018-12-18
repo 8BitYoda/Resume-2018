e676ce4e68ca7c49fa4ad642fc30bbe30dc43c71
Martin Hutchens <htnitram@gmail.com>
2018-12-18 14:21:11 -0500

    updated assets; css tweaks

--- src/app/resume/resume.component.ts
+++ src/app/resume/resume.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { IResumeData } from './data-service/resume-data';
 
 @Component({
@@ -6,6 +6,23 @@ import { IResumeData } from './data-service/resume-data';
   templateUrl: './resume.component.html',
   styleUrls: ['./resume.component.scss']
 })
-export class ResumeComponent {
+export class ResumeComponent implements OnChanges {
   @Input() rData: IResumeData;
+  skillList: Set<string> = new Set();
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (this.rData) {
+      const _jobs = this.rData.jobs;
+
+      if (_jobs) {
+        for (const i in _jobs) {
+          if (_jobs[i].skills) {
+            _jobs[i].skills.forEach(skill => {
+              this.skillList.add(skill.name);
+            });
+          }
+        }
+      }
+    }
+  }
 }
